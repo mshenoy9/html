4744b96cae910d338139cfbb61d92eb2e14dd955
Manjusha M <mshenoy9@gmail.com>
2015-06-28 22:35:37 +0200

    added bhagawad gita database entering format

---
+++ bhagavat.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<style>
+[class*='col-'] {
+  float: left;
+}
+body {
+    background-color:orange;
+}
+
+
+.col-2-3 {
+        margin-right:2;
+        float: left;
+         margin-left:5;
+  width: 58%;
+  
+}
+.col-1-3 {
+        margin-left:2;
+        margin-right:5;
+        float: right;
+  width: 42%;
+}
+.grid {
+  width: 100%;
+  overflow:hidden;
+   height:100%;
+}
+
+
+@media screen and (max-width: 1000px) {
+    body {
+        background-color: white;    
+    }
+	.
+	.col-2-3 {
+  		width: 100%;
+	  }
+	.col-1-3 {
+	  width: 100%;
+	}}
+</style>
+</head>
+<body>
+<div class=".col-2-3">
+
+     
+
+</div>
+    
+
+  <div class="grid">
+<center><img src="bhagavatimage2.jpg"width="500"height="200">
+</center>
+<hr color="red">
+ <br>
+</div>
+
+<div class=".col-2-3">
+
+<h1 style="color:red">BHAGAVAT-GITA </h1>
+
+<a href="sloka.php"style="text-decoration:none">CHAPTER1 : Arjun Vishad Yog &nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER2 : Sankhya Yog &nbsp; &nbsp;</a> <br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER3 : Karma Yog &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER4 : Gyan-Karma-Sanyas Yog &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER5 : Karma-sanyas Yoga </a><br>
+ <a href="sloka.php"style="text-decoration:none">CHAPTER6 : Dhyan Yog &nbsp; &nbsp;</a> <br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER7 : Gyan-Vigyan Yog &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER8 : Akshar-Brahma Yog
+ &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER9 : Raj Vidya Raj Guyha Yog </a><br>
+    <a href="sloka.php"style="text-decoration:none">CHAPTER10 : Vibhuti Yog &nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER11 : Vishvaroop Darshan Yog &nbsp; &nbsp;</a> <br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER12 : Bhakti yoga &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER13 : Kshetra-Kshetragya Vibhag Yog &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER14 : Gun-tray Vibhag Yog </a><br>
+
+       <a href="sloka.php"style="text-decoration:none">CHAPTER15 : Purushottam Yog &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER16 : Daivasur-Sampdvibhag Yog
+ &nbsp;&nbsp;</a><br>
+       <a href="sloka.php"style="text-decoration:none">CHAPTER17 : Shradhatray Vibhag Yog </a><br>
+  <a href="sloka.php"style="text-decoration:none">CHAPTER18 : Moksha-Sanyas Yog </a>  
+</div>
+</body>
+</html>
---
+++ sloka.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head><style>
+[class*='col-'] {
+  float: left;
+}
+body {
+    background-color:orange;
+}
+
+
+.col-2-3 {
+        margin-right:2;
+        float: left;
+         margin-left:5;
+  width: 58%;
+  
+}
+.col-1-3 {
+        margin-left:2;
+        margin-right:5;
+        float: right;
+  width: 42%;
+}
+.grid {
+  width: 100%;
+  overflow:hidden;
+   height:100%;
+}
+
+
+@media screen and (max-width: 1000px) {
+    body {
+        background-color: orange;    
+    }
+	.
+	.col-2-3 {
+  		width: 100%;
+	  }
+	.col-1-3 {
+	  width: 100%;
+	}}
+</style>
+</head></head>
+<body>
+
+<h1 style="color:red">BHAGAVAT-GITA ShlokaS </h1>
+<div class="grid">
+<a href="databaseenter.php"style="text-decoration:none">Shloka 1 &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 2  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 3 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 4  &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 5  &nbsp;&nbsp;</a><br>
+ <a href="databaseenter.php"style="text-decoration:none">Shloka6  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 7  &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 8 
+ &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 9 </a><br>
+    <a href="databaseenter.php"style="text-decoration:none">Shloka 10  &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 11  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 12 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 13 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 14 </a><br>
+
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 15&nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 16
+ &nbsp;&nbsp;</a><br>
+       <a href="Shloka.php"style="text-decoration:none">Shloka 17 </a><br>
+  <a href="databaseente.php"style="text-decoration:none">Shloka 18 </a>  <br>
+<a href="databaseenter.php"style="text-decoration:none">Shloka 19 &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 20  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 21 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 22  &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 23  &nbsp;&nbsp;</a><br>
+ <a href="databaseenter.php"style="text-decoration:none">Shloka 24  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka25  &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka26 
+ &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 27 </a><br>
+    <a href="databaseenter.php"style="text-decoration:none">Shloka 28  &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 29  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 30 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 31 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 32 </a><br>
+
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 33&nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 34
+ &nbsp;&nbsp;</a><br>
+       <a href="Shloka.php"style="text-decoration:none">Shloka 35 </a><br>
+  <a href="databaseente.php"style="text-decoration:none">Shloka 36 </a><br>  
+<a href="databaseenter.php"style="text-decoration:none">Shloka 37  &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 38 
+ &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 39 </a><br>
+    <a href="databaseenter.php"style="text-decoration:none">Shloka 40  &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 41  &nbsp; &nbsp;</a> <br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 42 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 43 &nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 44 </a><br>
+
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 45&nbsp;&nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 46
+ &nbsp;&nbsp;</a><br>
+       <a href="Shloka.php"style="text-decoration:none">Shloka 47 </a><br>
+  <a href="databaseente.php"style="text-decoration:none">Shloka 48 </a>  <br>
+<a href="databaseenter.php"style="text-decoration:none">Shloka 49 &nbsp;</a><br>
+       <a href="databaseenter.php"style="text-decoration:none">Shloka 50  &nbsp; &nbsp;</a> <br>
+      
+</div>
+
+</body>
+</html>
